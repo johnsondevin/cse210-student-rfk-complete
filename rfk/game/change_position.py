c2a537d88de980d3789436cf4e34e076842dbeb9
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-11-11 15:55:12 -0700

    Create change_position.py

---
+++ rfk/game/change_position.py
@@ -0,0 +1,9 @@
+from game.action import Action
+from random import randint
+
+class Change(Action):
+    def __init__(self):
+        pass
+
+    def execute(self, cast):
+        return super().execute(cast)
