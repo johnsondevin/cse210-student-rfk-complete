15f0bb818122a016658f311c23a0c2b5366dbaef
johnsondevin <91571848+johnsondevin@users.noreply.github.com>
2021-11-11 16:04:52 -0700

    Update change_position.py

--- rfk/game/change_position.py
+++ rfk/game/change_position.py
@@ -1,8 +1,14 @@
 from game.action import Action
+from random import randint
+from game.point import Point
+from game import constants
 
 class Change(Action):
-    def __init__(self):
-        pass
-
     def execute(self, cast):
-        return super().execute(cast)
+        if randint(0,50) != 50:
+            return
+        for artifact in cast["artifact"]:
+            x = randint(0, constants.MAX_X - 1)
+            y = randint(1, constants.MAX_Y - 1)
+            position = Point(x, y)
+            artifact.set_position(position)
